db490d6c1fbb908839d96218580c21f228bf9725
ting9093wei <ting9093wei@gmail.com>
2021-03-17 21:27:09 +0800

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(File.OpenRead("11002.csv"));
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(",");
+
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    Console.Write(values[i] + " ");
+                }
+                Console.Write("\n");
+            }
+                
+                
+        }
+    }
+}
